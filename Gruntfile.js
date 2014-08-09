fde132b15563b7bfcc6ba3a703a00d514b8b2468
Wildhoney <adam.timberlake@gmail.com>
2014-08-09 16:39:10 +0100

    Added JSHint

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function(grunt) {
 
 //    grunt.loadNpmTasks('grunt-karma');
 //    grunt.loadNpmTasks('grunt-contrib-uglify');
-//    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 //    grunt.loadNpmTasks('grunt-contrib-compress');
 //    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
