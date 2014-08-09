e721278a1d42781c40d2184c16c0797ff1081d3c
Wildhoney <adam.timberlake@gmail.com>
2014-08-09 19:17:53 +0100

    Added slash to public dir

--- example/server.js
+++ example/server.js
@@ -6,7 +6,7 @@
         app         = express(),
         server      = require('http').createServer(app);
 
-    app.use(express.static(__dirname));
+    app.use(express.static(__dirname + '/'));
     server.listen($process.env.PORT || 3507);
 
 })(process);
