90ad029b968e3005be942e1b75c0a1d97d790b26
Wildhoney <adam.timberlake@gmail.com>
2015-11-04 21:50:26 +0000

    Build :package: relates to #5

--- dist/ng-imgur.js
+++ dist/ng-imgur.js
@@ -109,7 +109,7 @@
 
                 // Strip the image type from the base64 data.
                 var base64Data  = event.target.result.split(',')[1],
-                    headerModel = { Authorization: 'Client-ID ' + imgurOptions.API_KEY },
+                    headerModel = { Authorization: imgurOptions.API_KEY },
                     dataModel   = { image: base64Data };
 
                 // Issue the request to upload the image data.
